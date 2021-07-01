1bd3b5d9afd684e491e5193f67e19be5bff050ff
zafuzi <zacharyfoutz@gmail.com>
2021-06-30 21:41:55 -0700

    added README

---
+++ README.txt
@@ -0,0 +1,21 @@
+_______________________________________________________________________________
+
+  
+                                MICRO TS
+
+                         TYPESCRIPT, LESS, HTML
+
+_______________________________________________________________________________
+
+
+
+
+- about -
+
+Micro TS is a simple template for starting a new project that requires Typescript, LESS, and HTML
+
+- structure -
+    src
+        /index.ts - main "javascript"
+    
+    compiled code goes to dist/
---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("loaded");
+});
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWRlbW8vLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxRQUFRLENBQUMsZ0JBQWdCLENBQUMsa0JBQWtCLEVBQUU7SUFFMUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLENBQUMsQ0FBQztBQUMxQixDQUFDLENBQUMsQ0FBQyIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcihcIkRPTUNvbnRlbnRMb2FkZWRcIiwgKCkgPT4gXG57XG4gICAgY29uc29sZS5sb2coXCJsb2FkZWRcIik7XG59KTsiXSwic291cmNlUm9vdCI6IiJ9
